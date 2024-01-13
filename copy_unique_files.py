18eac9c37d08a6fc56a2533d0f453cef7d0a3c6d
Jeffrey Green <jeffreygreen@Jeffreys-MBP.ht.home>
2024-01-12 23:15:12 -0600

    Retrieve the list of files in source and destination folders as well as identifying the files that are in source but not in destination

--- copy_unique_files.py
+++ copy_unique_files.py
@@ -2,4 +2,10 @@ import os
 import shutil
 
 def copy_unique_files(source_folder, destination_folder):
+
+    # Get the list of files in source and destination folders
+    source_files = set(os.listdir(source_folder))
+    destination_files = set(os.listdir(destination_folder))
+
+    new_files = source_files - destination_files
     
