4c2dd7134bf617a0b3ef294342e55e45cee4f9f2
Jeffrey Green <jeffreygreen@Jeffreys-MBP.ht.home>
2024-01-12 23:18:44 -0600

    copy new files from source to destination

--- copy_unique_files.py
+++ copy_unique_files.py
@@ -7,5 +7,14 @@ def copy_unique_files(source_folder, destination_folder):
     source_files = set(os.listdir(source_folder))
     destination_files = set(os.listdir(destination_folder))
 
+    # Identify new files (files in source but not in destination)
     new_files = source_files - destination_files
-    
+
+    # Copy new files from source to destination
+    for file_name in new_files:
+        source_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(destination_folder, file_name)
+        shutil.copy2(source_path, destination_path)
+        print(f"Copied: {file_name}")
+        
+
